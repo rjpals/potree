fe9c8d4dc6e84f49c3aec1351331fdf3a23f36de
Ryan Pals <ryan@hobu.co>
2019-07-30 16:15:49 -0500

    Get rid of .then()

--- examples/movie.js
+++ examples/movie.js
@@ -12,30 +12,30 @@ const tickDisplayedPointCloud = (i, ms, step, repititions) => {
 const startMovie = () => tickDisplayedPointCloud(0, 1500, 1, Infinity)
 
 // takes an array of objects {name: "lion", path: "./lion/ept.json"} 
-const loadPointcloudsInOrder = (resources) => {
-    console.log('entry')
+const loadPointcloudsInOrder = async (resources) => {
+
     const pcPromises = resources.map( ({ name, path }) => {
         return new Promise(
             (resolve) => Potree.loadPointCloud(path, name, resolve)
         ) 
     })
 
-    Promise.all(pcPromises).then( (events) => {
-        const pointclouds = events.map( (e) => e.pointcloud)
-        for(let pointcloud of pointclouds) {
-            pointcloud.visible = false
-            let material = pointcloud.material;
-            material.size = 3;
-            material.pointSizeType = Potree.PointSizeType.FIXED;
+    const events = await Promise.all(pcPromises)
+    const pointclouds = events.map( (e) => e.pointcloud)
+    const scene = window.viewer.scene
+    for(let pointcloud of pointclouds) {
+        pointcloud.visible = false
+        let material = pointcloud.material;
+        material.size = 3;
+        material.pointSizeType = Potree.PointSizeType.FIXED;
+        material.pointColorType = Potree.PointColorType.ELEVATION
+        material.elevationRange = [0, 180]
 
-            window.viewer.scene.addPointCloud(pointcloud)
+        await scene.addPointCloud(pointcloud)
 
-            //viewer.fitToScreen(0.5);
-        }
-        viewer.scene.view.position.set(-4234485.586, 9990252.213, 36176.402)
-        viewer.scene.view.lookAt(new THREE.Vector3(-4246615.561, 9974709.709, 60.068));
-        viewer.scene.pointclouds.forEach(pc => pc.material.pointColorType = Potree.PointColorType.ELEVATION)
-        viewer.scene.pointclouds.forEach(pc => pc.material.elevationRange = [8, 188])
-    })
+        //viewer.fitToScreen(0.5);
+    }
+    scene.view.position.set(-4234485.586, 9990252.213, 36176.402)
+    scene.view.lookAt(new THREE.Vector3(-4246615.561, 9974709.709, 60.068));
 }
 
