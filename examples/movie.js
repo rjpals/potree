7bd8beb02b8d2d063fc9644adc16ab2f2575897f
Ryan Pals <ryan@hobu.co>
2019-07-30 10:40:31 -0500

    Cleanup

--- examples/movie.js
+++ examples/movie.js
@@ -6,6 +6,33 @@ const tickDisplayedPointCloud = (i, pcs) => {
     setTimeout( () => tickDisplayedPointCloud((i + 1) % pcs.length, pcs), 250)
 }
 
+// takes an array of objects {name: "lion", path: "./lion/ept.json"} 
+const loadPointcloudsInOrder = (resources) => {
+    console.log('entry')
+    const pcPromises = resources.map( ({ name, path }) => {
+        return new Promise(
+            (resolve) => Potree.loadPointCloud(path, name, resolve)
+        ) 
+    })
+
+    Promise.all(pcPromises).then( (events) => {
+        const pointclouds = events.map( (e) => e.pointcloud)
+        for(let pointcloud of pointclouds) {
+            pointcloud.visible = false
+            let material = pointcloud.material;
+            material.size = 1;
+            material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
+
+            window.viewer.scene.addPointCloud(pointcloud)
+
+            viewer.fitToScreen(0.5);
+        }
+        //go()
+        tickDisplayedPointCloud(0, window.viewer.scene.pointclouds)
+    })
+}
+
+
 const go = () => {
     const pcs = window.viewer.scene.pointclouds
     console.log("Testing testing 123")
@@ -13,5 +40,3 @@ const go = () => {
     //function to hide #i pointcloud and show #i+1 pointcloud
     tickDisplayedPointCloud(0, pcs)
 }
-
-setTimeout(go, 5000)
