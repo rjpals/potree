0571e76248a83d88f6f48c0edeaadf90ec1c4497
Ryan Pals <ryan@hobu.co>
2019-07-30 16:03:13 -0500

    Move movieResources into seperate file

--- examples/movie.js
+++ examples/movie.js
@@ -1,9 +1,15 @@
-const tickDisplayedPointCloud = (i) => {
+const tickDisplayedPointCloud = (i, ms, step, repititions) => {
+    if(repititions < 1) return
     const pcs = window.viewer.scene.pointclouds
-    pcs[i].visible = false
-    pcs[(i+1) % pcs.length].visible = true
-    setTimeout( () => tickDisplayedPointCloud((i + 1) % pcs.length), 250)
+    console.log(pcs.length)
+    if(pcs.length > 0) {
+        pcs[(i+step) % pcs.length].visible = true
+        pcs[i].visible = false
+    }
+    setTimeout( () => tickDisplayedPointCloud((i + step) % pcs.length, ms, step, repititions - 1), ms)
 }
+
+const startMovie = () => tickDisplayedPointCloud(0, 1500, 1, Infinity)
 
 // takes an array of objects {name: "lion", path: "./lion/ept.json"} 
 const loadPointcloudsInOrder = (resources) => {
@@ -19,14 +25,17 @@ const loadPointcloudsInOrder = (resources) => {
         for(let pointcloud of pointclouds) {
             pointcloud.visible = false
             let material = pointcloud.material;
-            material.size = 1;
-            material.pointSizeType = Potree.PointSizeType.ADAPTIVE;
+            material.size = 3;
+            material.pointSizeType = Potree.PointSizeType.FIXED;
 
             window.viewer.scene.addPointCloud(pointcloud)
 
-            viewer.fitToScreen(0.5);
+            //viewer.fitToScreen(0.5);
         }
-        tickDisplayedPointCloud(0)
+        viewer.scene.view.position.set(-4234485.586, 9990252.213, 36176.402)
+        viewer.scene.view.lookAt(new THREE.Vector3(-4246615.561, 9974709.709, 60.068));
+        viewer.scene.pointclouds.forEach(pc => pc.material.pointColorType = Potree.PointColorType.ELEVATION)
+        viewer.scene.pointclouds.forEach(pc => pc.material.elevationRange = [8, 188])
     })
 }
 
