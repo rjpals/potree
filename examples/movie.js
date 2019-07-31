3d0206f236b83ee31ae83d20f38415b92cc1b1f5
Ryan Pals <ryan@hobu.co>
2019-07-31 14:55:28 -0500

    Cleanup promises

--- examples/movie.js
+++ examples/movie.js
@@ -20,8 +20,6 @@ const startMovie = () => {
 
 // takes an array of objects {name: "lion", path: "./lion/ept.json"} 
 const loadPointcloudsInOrder = async (resources) => {
-    const scene = window.viewer.scene
-
     const pcPromises = resources.map( ({ name, path }) => {
         return new Promise(
             (resolve) => Potree.loadPointCloud(path, name, resolve)
@@ -30,19 +28,27 @@ const loadPointcloudsInOrder = async (resources) => {
 
     const events = await Promise.all(pcPromises)
     const pointclouds = events.map( (e) => e.pointcloud)
-    for(let pointcloud of pointclouds) {
+    pointclouds.forEach( (pc) => window.viewer.scene.addPointCloud(pc))
+
+    return pointclouds
+}
+
+
+const glacierInit = async () => {
+    const pointclouds = await loadPointcloudsInOrder(window.movieResources)
+    for(const pointcloud of pointclouds) {
         pointcloud.visible = false
-        let material = pointcloud.material;
+
+        const material = pointcloud.material;
         material.size = 3;
         material.pointSizeType = Potree.PointSizeType.FIXED;
         material.pointColorType = Potree.PointColorType.ELEVATION
         material.elevationRange = [0, 180]
-
-        scene.addPointCloud(pointcloud)
-
-        //viewer.fitToScreen(0.5);
     }
+
+    const scene = window.viewer.scene
     scene.view.position.set(-4234485.586, 9990252.213, 36176.402)
     scene.view.lookAt(new THREE.Vector3(-4246615.561, 9974709.709, 60.068));
+    startMovie()
 }
 
